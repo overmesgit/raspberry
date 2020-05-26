9bd62740e97d81e01f78619782670bbefde3601e
overmes <artem.bezu@localfolio.co.jp>
2020-05-26 20:53:35 +0900

    device var

--- tet.py
+++ tet.py
@@ -1,9 +1,11 @@
 # import evdev
+import os
+input_dev = os.environ['INPUT']
 from evdev import InputDevice, categorize, ecodes
 
 # creates object 'gamepad' to store the data
 # you can call it whatever you like
-gamepad = InputDevice('/dev/input/event3')
+gamepad = InputDevice(f'/dev/input/{input_dev}')
 
 # prints out device info at start
 print(gamepad)
